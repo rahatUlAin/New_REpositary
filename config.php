6c592abf1e727e4cd681594ac6c27fd71cf61f5a
Rahat-Ul-Ain <rahatulain880@gmail.com>
2020-02-04 17:05:08 +0500

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 	session_start();
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("153231444624-t69obookdurt00l3dlf38fjhvjitb7ih.apps.googleusercontent.com");
-	$gClient->setClientSecret("1YLQsEji8Lva9DxSFVSvyOcc");
+	$gClient->setClientId("smth");
+	$gClient->setClientSecret("smth");
 	$gClient->setApplicationName("CPI Login Tutorial");
 	$gClient->setRedirectUri("http://localhost/GoogleLogin/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
